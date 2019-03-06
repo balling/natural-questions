2150312c3f3c97411a5f339fe49b6ddb93959284
Yiling Chen <balling.xo@gmail.com>
2019-03-05 22:29:03 -0800

    bug fix

--- train.py
+++ train.py
@@ -140,11 +140,11 @@ def main():
     model.train()
     for _ in trange(int(args.num_train_epochs), desc="Epoch"):
         for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
-                if n_gpu == 1:
+                if len(gpu_ids) == 1:
                     batch = tuple(t.to(device) for t in batch) # multi-gpu does scattering it-self
                 input_ids, input_mask, segment_ids, start_positions, end_positions = batch
                 loss = model(input_ids, segment_ids, input_mask, start_positions, end_positions)
-                if n_gpu > 1:
+                if len(gpu_ids) > 1:
                     loss = loss.mean() # mean() to average on multi-gpu.
                 if args.gradient_accumulation_steps > 1:
                     loss = loss / args.gradient_accumulation_steps
