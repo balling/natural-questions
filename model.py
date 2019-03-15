14d57b7c78a00e6a5e2576360d86923d7562e401
Yiling Chen <balling.xo@gmail.com>
2019-03-14 20:48:57 -0700

    more complex output layer

--- model.py
+++ model.py
@@ -2,76 +2,40 @@ import torch
 from torch import nn
 from torch.nn import CrossEntropyLoss
 import torch.nn.functional as F
-from pytorch_pretrained_bert.modeling import BertModel, BertPreTrainedModel#, BertConfig, WEIGHTS_NAME, CONFIG_NAME
+from pytorch_pretrained_bert.modeling import BertModel, BertPreTrainedModel
 import numpy as np
 
+
 class BertForNQ(BertPreTrainedModel):
-    """BERT model for Question Answering (span extraction).
-    This module is composed of the BERT model with a linear layer on top of
-    the sequence output that computes start_logits and end_logits
-
-    Params:
-        `config`: a BertConfig class instance with the configuration to build a new model.
-
-    Inputs:
-        `input_ids`: a torch.LongTensor of shape [batch_size, sequence_length]
-            with the word token indices in the vocabulary(see the tokens preprocessing logic in the scripts
-            `extract_features.py`, `run_classifier.py` and `run_squad.py`)
-        `token_type_ids`: an optional torch.LongTensor of shape [batch_size, sequence_length] with the token
-            types indices selected in [0, 1]. Type 0 corresponds to a `sentence A` and type 1 corresponds to
-            a `sentence B` token (see BERT paper for more details).
-        `attention_mask`: an optional torch.LongTensor of shape [batch_size, sequence_length] with indices
-            selected in [0, 1]. It's a mask to be used if the input sequence length is smaller than the max
-            input sequence length in the current batch. It's the mask that we typically use for attention when
-            a batch has varying length sentences.
-        `start_positions`: position of the first token for the labeled span: torch.LongTensor of shape [batch_size].
-            Positions are clamped to the length of the sequence and position outside of the sequence are not taken
-            into account for computing the loss.
-        `end_positions`: position of the last token for the labeled span: torch.LongTensor of shape [batch_size].
-            Positions are clamped to the length of the sequence and position outside of the sequence are not taken
-            into account for computing the loss.
-
-    Outputs:
-        if `start_positions` and `end_positions` are not `None`:
-            Outputs the total_loss which is the sum of the CrossEntropy loss for the start and end token positions.
-        if `start_positions` or `end_positions` is `None`:
-            Outputs a tuple of start_logits, end_logits which are the logits respectively for the start and end
-            position tokens of shape [batch_size, sequence_length].
-
-    Example usage:
-    ```python
-    # Already been converted into WordPiece token ids
-    input_ids = torch.LongTensor([[31, 51, 99], [15, 5, 0]])
-    input_mask = torch.LongTensor([[1, 1, 1], [1, 1, 0]])
-    token_type_ids = torch.LongTensor([[0, 0, 1], [0, 1, 0]])
-
-    config = BertConfig(vocab_size_or_config_json_file=32000, hidden_size=768,
-        num_hidden_layers=12, num_attention_heads=12, intermediate_size=3072)
-
-    model = BertForQuestionAnswering(config)
-    start_logits, end_logits = model(input_ids, token_type_ids, input_mask)
-    ```
-    """
     def __init__(self, config):
         super(BertForNQ, self).__init__(config)
         self.bert = BertModel(config)
-        # TODO check with Google if it's normal there is no dropout on the token classifier of SQuAD in the TF version
-        # self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.qa_outputs = nn.Linear(config.hidden_size, 2)
-        self.type_output = nn.Linear(config.hidden_size, 5)
+
+        self.type_rnn = torch.nn.LSTM(
+            config.hidden_size, config.hidden_size, batch_first=True, bidirectional=True)
+        self.type_output = nn.Linear(2*config.hidden_size, 5)
+        self.start_rnn = torch.nn.LSTM(
+            3*config.hidden_size, config.hidden_size, batch_first=True, bidirectional=True)
+        self.start_output = nn.Linear(2*config.hidden_size, 1)
+        self.end_rnn = torch.nn.LSTM(
+            5*config.hidden_size, config.hidden_size, batch_first=True, bidirectional=True)
+        self.end_output = nn.Linear(2*config.hidden_size, 1)
         self.apply(self.init_bert_weights)
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, start_positions=None, end_positions=None, ans_types=None):
-        sequence_output, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-        logits = self.qa_outputs(sequence_output)
-        start_logits, end_logits = logits.split(1, dim=-1)
-        start_logits = start_logits.squeeze(-1)
-        end_logits = end_logits.squeeze(-1)
-        type_logits = self.type_output(pooled_output)
+        sequence_output, _ = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
 
-        mask = 1-attention_mask.byte()
-        start_logits=start_logits.masked_fill_(mask, -np.inf)
-        end_logits=end_logits.masked_fill_(mask, -np.inf)
+        type_rnn_out, (hn, cn) = self.type_rnn(sequence_output)
+        hn = hn.permute(1,0,2).contiguous().view(hn.size(1), -1)
+        type_logits = self.type_output(hn)
+
+        start_sequence = torch.cat([sequence_output, type_rnn_out], dim=-1)
+        start_rnn_out, _ = self.start_rnn(start_sequence)
+        start_logits = self.start_output(start_rnn_out).squeeze(-1)
+
+        end_sequence = torch.cat([start_sequence, start_rnn_out], dim=-1)
+        end_rnn_out, _ = self.end_rnn(end_sequence)
+        end_logits = self.end_output(end_rnn_out).squeeze(-1)
 
         if start_positions is not None and end_positions is not None:
             # If we are on multi-GPU, split add a dimension
